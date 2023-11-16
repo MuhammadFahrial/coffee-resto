1ab450f54a863c39019cb2ce9f8bbbc697ab2bfb
unknown <rialjhe111@gmail.com>
2023-11-16 18:22:28 +0800

    add:cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,53 @@
 import React, { useState } from "react";
 import { Search, ShoppingCart, Menu } from "react-feather";
+import Cart from "../Cart/Cart";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
   const [cart, setCart] = useState(false);
+
+  const handleClickToggle = () => {
+    setToggle(!toggle);
+    if (toggle == false) {
+      if (openSearch == true) {
+        setOpenSearch(openSearch == false);
+        if (cart == true) {
+          setCart(cart == false);
+        }
+      } else if (cart == true) {
+        setCart(cart == false);
+      }
+    }
+  };
+
+  const handleClickSearch = () => {
+    setOpenSearch(!openSearch);
+    if (openSearch == false) {
+      if (toggle == true) {
+        setToggle(toggle == false);
+        if (cart == true) {
+          setCart(cart == false);
+        }
+      } else if (cart == true) {
+        setCart(cart == false);
+      }
+    }
+  };
+
+  const handleClickCart = () => {
+    setCart(!cart);
+    if (cart == false) {
+      if (toggle == true) {
+        setToggle(toggle == false);
+        if (openSearch == true) {
+          setOpenSearch(openSearch == false);
+        }
+      } else if (openSearch == true) {
+        setOpenSearch(openSearch == false);
+      }
+    }
+  };
 
   return (
     <>
@@ -21,15 +65,24 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-extra">
-          <a href="#" id="search">
+          <a id="search" onClick={() => handleClickSearch()}>
             <Search />
           </a>
-          <a href="#" id="shopping-cart" onClick={() => setCart(!cart)}>
+          <a id="shopping-cart" onClick={() => handleClickCart()}>
             <ShoppingCart />
           </a>
-          <a id="hamburger-menu" onClick={() => setToggle(!toggle)}>
+          <a id="hamburger-menu" onClick={() => handleClickToggle()}>
             <Menu />
           </a>
+        </div>
+        <div className={openSearch ? "search-form active" : "search-form"}>
+          <input type="search" id="search-box" placeholder="search here..." />
+          <label htmlFor="search-box">
+            <Search />
+          </label>
+        </div>
+        <div className={cart ? "shopping-cart active" : "shopping-cart"}>
+          <Cart />
         </div>
       </nav>
     </>
