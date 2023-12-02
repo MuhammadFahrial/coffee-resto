0aeea2899770caac7a7a1cb5dfe07b27030971e4
unknown <rialjhe111@gmail.com>
2023-12-02 09:47:25 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Customers from "./components/Customers/Customers";
 import ExploreProduct from "./components/ExploreProduct/ExploreProduct";
 import Footer from "./components/Footers/Footer";
 import Hero from "./components/Hero/Hero";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import OurServices from "./components/Services/Services";
 import SpecialProduct from "./components/SpecialProduct/SpecialProduct";
 
